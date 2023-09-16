5c14603905540801a824483d5ed058cf5c5cd718
Nagesh <satyam9mzp@gamil.com>
2023-09-16 21:22:22 +0530

    okay ise krk try kr lete h

---
+++ app/page.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Main from '@/Components/FirstPage/main'
+import Footer from '@/Components/Footer';
+
+const page = () => {
+  return <>
+    <Main/>
+    <Footer/>
+  </>
+}
+
+export default page;
